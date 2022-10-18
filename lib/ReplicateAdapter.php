23a55473ad372a373846d0012c517c738b669551
Antonio J. García Lagar <aj@garcialagar.es>
2022-10-18 10:42:22 +0200

    Drop support for PHP < 7.4

--- lib/ReplicateAdapter.php
+++ lib/ReplicateAdapter.php
@@ -15,7 +15,7 @@ class ReplicateAdapter extends AjglReplicateAdapter
      */
     public function __construct(AdapterInterface $source, AdapterInterface $replica)
     {
-        @trigger_error('Since ajgl/flysystem-replicate 1.0.2: The "League\Flysystem\Replicate\ReplicateAdapter" class is deprecated, use "Ajgl\Flysystem\Replicate\ReplicateAdapter" instead.', \E_USER_DEPRECATED);
+        @trigger_error(sprintf('Since ajgl/flysystem-replicate 1.0.2: The "%s" class is deprecated, use "%s" instead.', self::class, AjglReplicateAdapter::class), \E_USER_DEPRECATED);
         $this->source = $source;
         $this->replica = $replica;
     }
